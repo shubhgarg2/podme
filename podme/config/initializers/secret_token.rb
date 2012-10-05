48343fc0427cda189a1fab4d345208b7f0c649ea
Vikas Sahu <vikassahu2904@gmail.com>
2012-10-05 23:58:08 +0530

    Lets begin

---
+++ podme/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podme::Application.config.secret_token = 'b208f613ff77dfdba2b3ad03c05d8e9a9b9490e4289c143af31c579604f9052ee56b5d636c90d62e8a84e926a99a51fc41a3686718e8187769d2075a52c90fd3'
